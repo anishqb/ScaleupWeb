d249fb3682d5b18553ac930f397e6a2326229a4e
qbuser <anishv@qburst.com>
2020-03-05 15:48:11 +0530

    Instructions updated

--- src/app/instruction/instruction.component.ts
+++ src/app/instruction/instruction.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiService } from '../service/api.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-instruction',
@@ -8,19 +10,34 @@ import { Router } from '@angular/router';
 })
 export class InstructionComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
-  isAccept:boolean;
+  constructor(private router: Router, private formBuilder: FormBuilder, private apiService: ApiService) { }
+  
+  qTypeForm: FormGroup;
+  isAccept: boolean;
+  Types: any[] = [];
 
   ngOnInit(): void {
-    this.isAccept=false;
+    this.isAccept = false;
+    var response = this.apiService.getQuestionTypes(); 
+
+    if (response.status == 200) {
+      response.result.forEach(element => {
+        this.Types.push(element);
+      });
+    }
+
   }
 
-  onContinue(){
+  onContinue() {
     if (!this.isAccept) {
       return;
     }
     this.router.navigate(["app-question"]);
+  }
+
+  changeType(typ:any){
+
+    alert(typ);
   }
 
 }
--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -13,13 +13,13 @@ export class ApiService {
     constructor(private http: HttpClient) { }
 
     baseUrl: string = 'http://localhost:8080/';
-    yy:ApiResponse;
+    yy: ApiResponse;
 
     getQuestions(): Observable<ApiResponse> {
 
         var xx = this.readQuestions();
-        var tt: Observable< ApiResponse>;
-        var yy:ApiResponse;
+        var tt: Observable<ApiResponse>;
+        var yy: ApiResponse;
         yy.status = 200;
         yy.result = xx;
         yy.url = this.baseUrl;
@@ -37,15 +37,60 @@ export class ApiService {
 
     //<------------------------------------------------------->//
     // Need to remove when api is completed
-    readQuestions():ApiResponse {
+    readQuestions(): ApiResponse {
         var abc = qstnJson;
-        let xx: ApiResponse={
-            message:"",
-            result:abc,
-            status:200,
-            url:this.baseUrl
+        let xx: ApiResponse = {
+            message: "",
+            result: abc,
+            status: 200,
+            url: this.baseUrl
 
-            };
+        };
         return xx;
+    }
+
+    getQuestionTypes(): ApiResponse {
+        var abc = [{
+            "Id": 1,
+            "Type": "Aptitude"
+
+        },
+        {
+            "Id": 2,
+            "Type": "General knowledge"
+        }]
+        let xx: ApiResponse = {
+            message: "",
+            result: abc,
+            status: 200,
+            url: this.baseUrl
+
+        };
+        return xx;
+    }
+    checkAnswers(data): ApiResponse {
+
+        var abc = qstnJson;
+        var score = 0;
+        data.forEach(element => {
+            abc.forEach(x => {
+                if (x.questionId == element.questionId) {
+
+                    if (x.c_answer == element.answer) {
+                        score = score + 10;
+                    }
+                }
+            })
+
+        });
+
+        let response: ApiResponse = {
+            message: "",
+            result: score,
+            status: 200,
+            url: this.baseUrl
+
+        };
+        return response;
     }
 }
